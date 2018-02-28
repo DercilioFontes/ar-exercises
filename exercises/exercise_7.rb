a447b417d90e866066406d54e22ea83021fde169
DercilioFontes <dercilioafontes@gmail.com>
2018-02-28 04:26:38 +0000

    Implemented Exercise 7 and tested.

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "Enter Store's name\n>"
+store_name = gets.chomp
+
+@new_store = Store.create(name: store_name)
+
+# Line below to test the validation method for both false apparel type
+# @new_store = Store.create(name: 'Store X', annual_revenue: 1_000_000, mens_apparel: false, womens_apparel: false)
+
+puts @new_store.errors.messages
